cd7a326c79759553fcf6fd5397d493ce1e3b450d
Meezzi <alswl-3016@naver.com>
2025-07-26 21:30:37 +0900

    feat: Product 모델 클래스 추가

---
+++ lib/data/models/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final String imageFile;
+  final String title;
+  final String description;
+  final String seller;
+  final int price;
+  final String address;
+  final int likes;
+  final int chats;
+
+  Product({
+    required this.imageFile,
+    required this.title,
+    required this.description,
+    required this.seller,
+    required this.price,
+    required this.address,
+    required this.likes,
+    required this.chats,
+  });
+}
