b76b99e4a0c6316ed9c4f8c46503fad2943a23ce
Meezzi <alswl-3016@naver.com>
2025-07-27 21:31:13 +0900

    feat: productDetailViewModelProvider 추가

--- lib/provider/products_provider.dart
+++ lib/provider/products_provider.dart
@@ -1,6 +1,13 @@
+import 'package:apple_market/data/models/product.dart';
+import 'package:apple_market/features/product/presentation/view_models/product_detail_view_model.dart';
 import 'package:apple_market/util/load_products.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final productsProvider = FutureProvider((ref) async {
   return await loadProducts();
 });
+
+final productDetailViewModelProvider = StateNotifierProvider.autoDispose
+    .family<ProductDetailViewModel, Product, Product>(
+      (ref, product) => ProductDetailViewModel(product),
+    );
