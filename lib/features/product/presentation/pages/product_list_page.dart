a44201f3e8f6aa23b39de44b3835da4ddf39f6ef
Meezzi <alswl-3016@naver.com>
2025-07-27 22:16:33 +0900

    refactor: 상품 리스트 상태를 ViewModel에서 처리하도록 수정

--- lib/features/product/presentation/pages/product_list_page.dart
+++ lib/features/product/presentation/pages/product_list_page.dart
@@ -17,8 +17,6 @@ class _ProductListPageState extends ConsumerState<ProductListPage> {
   final ScrollController _scrollController = ScrollController();
   bool _showScrollToTopButton = false;
 
-  List<Product> _productList = [];
-
   @override
   void initState() {
     super.initState();
@@ -27,7 +25,7 @@ class _ProductListPageState extends ConsumerState<ProductListPage> {
 
   @override
   Widget build(BuildContext context) {
-    final asyncProducts = ref.watch(productsProvider);
+    final asyncProducts = ref.watch(productListViewModelProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -46,22 +44,18 @@ class _ProductListPageState extends ConsumerState<ProductListPage> {
       ),
       body: asyncProducts.when(
         data: (products) {
-          if (_productList.isEmpty) {
-            _productList = List.from(products);
-          }
-
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
             child: ListView.separated(
               controller: _scrollController,
               separatorBuilder: (context, index) => const Divider(),
-              itemCount: _productList.length,
+              itemCount: products.length,
               itemBuilder: (context, index) {
-                final product = _productList[index];
+                final product = products[index];
                 return ProductCard(
+                  product: product,
                   onTap: () => goToProductDetail(context, product),
                   onLongPress: () => showAlertDialog(context, index),
-                  product: product,
                 );
               },
             ),
@@ -89,9 +83,7 @@ class _ProductListPageState extends ConsumerState<ProductListPage> {
             ),
             TextButton(
               onPressed: () {
-                setState(() {
-                  _productList.removeAt(index);
-                });
+                ref.read(productListViewModelProvider.notifier).removeAt(index);
                 Navigator.of(dialogContext).pop();
               },
               child: const Text('확인'),
