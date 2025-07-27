efc6f2e27b613c8482c7183ecb7f12d3b1e59f5b
Meezzi <alswl-3016@naver.com>
2025-07-27 19:47:10 +0900

    feat: ProductListPage에 상품 삭제 기능 구현

--- lib/features/product/presentation/pages/product_list_page.dart
+++ lib/features/product/presentation/pages/product_list_page.dart
@@ -17,6 +17,8 @@ class _ProductListPageState extends ConsumerState<ProductListPage> {
   final ScrollController _scrollController = ScrollController();
   bool _showScrollToTopButton = false;
 
+  List<Product> _productList = [];
+
   @override
   void initState() {
     super.initState();
@@ -43,40 +45,61 @@ class _ProductListPageState extends ConsumerState<ProductListPage> {
         scrollController: _scrollController,
       ),
       body: asyncProducts.when(
-        data: (products) => Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: ListView.separated(
-            controller: _scrollController,
-            separatorBuilder: (BuildContext context, int index) {
-              return Divider();
-            },
-            itemCount: products.length,
-            itemBuilder: (context, index) {
-              return ProductCard(
-                onTap: () => goToProductDetail(context, products[index]),
-                product: products[index],
-              );
-            },
-          ),
-        ),
+        data: (products) {
+          if (_productList.isEmpty) {
+            _productList = List.from(products);
+          }
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: ListView.separated(
+              controller: _scrollController,
+              separatorBuilder: (context, index) => const Divider(),
+              itemCount: _productList.length,
+              itemBuilder: (context, index) {
+                final product = _productList[index];
+                return ProductCard(
+                  onTap: () => goToProductDetail(context, product),
+                  onLongPress: () => showAlertDialog(context, index),
+                  product: product,
+                );
+              },
+            ),
+          );
+        },
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (err, stack) => Center(child: Text('오류가 발생했습니다.')),
       ),
     );
   }
 
-  @override
-  void dispose() {
-    _scrollController.removeListener(_scrollListener);
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _scrollListener() {
-    final shouldShow = _scrollController.offset > 0;
-    if (_showScrollToTopButton != shouldShow) {
-      setState(() => _showScrollToTopButton = shouldShow);
-    }
+  void showAlertDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('상품 삭제'),
+          content: const Text('이 상품을 삭제하시겠습니까?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(dialogContext).pop();
+              },
+              child: const Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _productList.removeAt(index);
+                });
+                Navigator.of(dialogContext).pop();
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void showNotificationSnackBar(BuildContext context) {
