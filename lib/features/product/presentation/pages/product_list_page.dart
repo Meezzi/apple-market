62c07ca4641b9a976f45ae2cd444b0fe4a55fa36
Meezzi <alswl-3016@naver.com>
2025-07-27 14:23:46 +0900

    feat: ProductListPage를 ConsumerWidget으로 변환하고 상품 데이터 표시

--- lib/features/product/presentation/pages/product_list_page.dart
+++ lib/features/product/presentation/pages/product_list_page.dart
@@ -1,15 +1,27 @@
+import 'package:apple_market/features/product/presentation/widgets/product_card.dart';
+import 'package:apple_market/provider/products_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductListPage extends StatefulWidget {
+class ProductListPage extends ConsumerWidget {
   const ProductListPage({super.key});
 
   @override
-  State<ProductListPage> createState() => _ProductListPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncProducts = ref.watch(productsProvider);
 
-class _ProductListPageState extends State<ProductListPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('르탄동'), centerTitle: false),
+      body: asyncProducts.when(
+        data: (products) => ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return ProductCard(product: products[index]);
+          },
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (err, stack) => Center(child: Text('오류가 발생했습니다.')),
+      ),
+    );
   }
 }
