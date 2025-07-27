df0a9bcf8de2fc0809b31699c917b0c3195ecab1
Meezzi <alswl-3016@naver.com>
2025-07-27 22:09:07 +0900

    feat: productListViewModelProvider 생성

--- lib/provider/products_provider.dart
+++ lib/provider/products_provider.dart
@@ -1,5 +1,6 @@
 import 'package:apple_market/data/models/product.dart';
 import 'package:apple_market/features/product/presentation/view_models/product_detail_view_model.dart';
+import 'package:apple_market/features/product/presentation/view_models/product_list_view_model.dart';
 import 'package:apple_market/util/load_products.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,7 +8,12 @@ final productsProvider = FutureProvider((ref) async {
   return await loadProducts();
 });
 
-final productDetailViewModelProvider = StateNotifierProvider.autoDispose
-    .family<ProductDetailViewModel, Product, Product>(
+final productListViewModelProvider =
+    AsyncNotifierProvider<ProductListViewModel, List<Product>>(() {
+      return ProductListViewModel();
+    });
+
+final productDetailViewModelProvider =
+    StateNotifierProvider.family<ProductDetailViewModel, Product, Product>(
       (ref, product) => ProductDetailViewModel(product),
     );
