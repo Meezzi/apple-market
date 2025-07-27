ae338d7e11a9e4d3c6bcad013bc05ef0e2170517
Meezzi <alswl-3016@naver.com>
2025-07-27 21:32:56 +0900

    refactor: ProductDetailPage를 ConsumerWidget으로 변경하고 상태 연동

--- lib/features/product/presentation/pages/product_detail_page.dart
+++ lib/features/product/presentation/pages/product_detail_page.dart
@@ -2,24 +2,28 @@ import 'package:apple_market/data/models/product.dart';
 import 'package:apple_market/features/product/presentation/widgets/product_bottom_bar.dart';
 import 'package:apple_market/features/product/presentation/widgets/product_info.dart';
 import 'package:apple_market/features/product/presentation/widgets/seller_info.dart';
+import 'package:apple_market/provider/products_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductDetailPage extends StatefulWidget {
+class ProductDetailPage extends ConsumerWidget {
   const ProductDetailPage({super.key, required this.product});
 
   final Product product;
 
   @override
-  State<ProductDetailPage> createState() => _ProductDetailPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productState = ref.watch(productDetailViewModelProvider(product));
+    final viewModel = ref.read(
+      productDetailViewModelProvider(product).notifier,
+    );
 
-class _ProductDetailPageState extends State<ProductDetailPage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('상품 상세')),
       bottomNavigationBar: ProductBottomBar(
-        price: widget.product.price,
+        price: productState.price,
+        isLiked: productState.isLiked,
+        onLikedTap: viewModel.toggleLike,
         onChatTap: () {
           showNotificationSnackBar(context, '준비 중입니다.');
         },
@@ -28,7 +32,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
         child: Column(
           children: [
             Image.asset(
-              'assets/product_images/${widget.product.imageFile}.png',
+              'assets/product_images/${productState.imageFile}.png',
               width: double.infinity,
               height: 260,
               fit: BoxFit.cover,
@@ -38,10 +42,10 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SellerInfo(product: widget.product),
+                  SellerInfo(product: productState),
                   Divider(height: 2, thickness: 2, color: Colors.grey[500]),
                   SizedBox(height: 8),
-                  ProductInfo(product: widget.product),
+                  ProductInfo(product: productState),
                 ],
               ),
             ),
