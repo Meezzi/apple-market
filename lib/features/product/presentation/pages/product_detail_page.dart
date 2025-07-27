8363f4cce25a8c58fbda4390efe2c967746e8a18
Meezzi <alswl-3016@naver.com>
2025-07-27 17:11:27 +0900

    refactor: SellerInfo 분리

--- lib/features/product/presentation/pages/product_detail_page.dart
+++ lib/features/product/presentation/pages/product_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:apple_market/data/models/product.dart';
 import 'package:apple_market/features/product/presentation/widgets/product_bottom_bar.dart';
 import 'package:apple_market/features/product/presentation/widgets/product_info.dart';
+import 'package:apple_market/features/product/presentation/widgets/seller_info.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetailPage extends StatelessWidget {
@@ -49,72 +50,5 @@ class ProductDetailPage extends StatelessWidget {
       ..showSnackBar(
         SnackBar(content: Text(content), duration: Duration(seconds: 2)),
       );
-  }
-}
-
-class SellerInfo extends StatelessWidget {
-  const SellerInfo({super.key, required this.product});
-
-  final Product product;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CircleAvatar(
-            radius: 24,
-            backgroundColor: Colors.deepOrangeAccent,
-            child: const Icon(Icons.person, color: Colors.white),
-          ),
-          const SizedBox(width: 12),
-
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.seller,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  product.address,
-                  style: const TextStyle(color: Colors.grey, fontSize: 14),
-                ),
-              ],
-            ),
-          ),
-
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                '39.3 ℃',
-                style: TextStyle(
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 4),
-              const Text(
-                '매너온도',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
-                  decoration: TextDecoration.underline,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
   }
 }
